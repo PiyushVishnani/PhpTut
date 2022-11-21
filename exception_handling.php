9a861b19df4ece67cc743f7da47fac0c1964da7d
piyush <vishnanipiyush@gmail.com>
2022-11-21 11:28:12 +0530

    commmit

--- exception_handling.php
+++ exception_handling.php
@@ -1,10 +1,30 @@
 <?php
 echo "<form action=$_SERVER[PHP_SELF] method='post'>
 <label>Enter numerator</label><input type='number' name='num'><br>
-<label>Enter denominator</label><input type='number' name='deno'><br>";
+<label>Enter denominator</label><input type='number' name='deno'><br>
+<input type='submit' name='result'>";
 if($_SERVER['REQUEST_METHOD']=="POST"){
     $num = $_POST['num'];
     $deno = $_POST['deno'];
-    echo "<input type='number' name='data' value=$num/$deno readonly">;
+    
+    class NumeratorIsGreater extends Exception{
+        function errorMessage(){
+            return $this->getMessage();
+        }
+    }
+    class NumeratorIsNegative extends Exception{
+        
+    }
+    class NumeratorIsZero extends Exception{
+        
+    }
+    try{
+        if($deno==0){
+            throw new NumeratorIsZero("Please provide a valid denominator");
+        }
+    }catch(NumeratorIsZero $e){
+            $e->getMessage(); 
+    }
+    
 }
 ?>
